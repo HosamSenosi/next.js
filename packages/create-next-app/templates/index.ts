537f0626fde3974b92d6956089987e088100d1b1
Ben Gubler <nebrelbug@gmail.com>
2025-08-22 19:40:11 -0700

    feat: create-next-app generates types during initialization

--- packages/create-next-app/templates/index.ts
+++ packages/create-next-app/templates/index.ts
@@ -1,4 +1,5 @@
 import { install } from "../helpers/install";
+import { runTypegen } from "../helpers/typegen";
 import { copy } from "../helpers/copy";
 
 import { async as glob } from "fast-glob";
@@ -302,6 +303,13 @@ export const installTemplate = async ({
   console.log();
 
   await install(packageManager, isOnline);
+  try {
+    console.log();
+    await runTypegen(packageManager);
+    console.log();
+  } catch (err) {
+    // Best effort: do not fail app creation if typegen fails
+  }
 };
 
 export * from "./types";
